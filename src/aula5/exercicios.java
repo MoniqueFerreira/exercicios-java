7747694c95b186f25cea6cc107ce0f68a5e50f5b
MoniqueFerreira <moniquesz23@gmail.com>
2018-09-10 22:28:44 -0300

    exercicios

---
+++ src/aula5/exercicios.java
@@ -0,0 +1,28 @@
+package aula5;
+
+public class exercicios {
+	public static void main (String[] args) {
+	
+	//exercicio 1	
+		double area;
+		int raio = 12;
+		double pi = 3.1415;
+		area = pi*Math.pow(raio, 2);
+	System.out.println(area);
+	
+	
+	// exercicio 2 
+		int result = 99 % 4;
+	System.out.println(result);
+	
+	
+	// exercicio 3
+		double multiplicacao = 50*0.5;
+	System.out.println(multiplicacao);
+	
+	
+	// exercicio 4
+		double divisao = 10/1.25;
+	System.out.println(divisao);
+	}
+}
